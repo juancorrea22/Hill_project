7cf6b735b4af39c9beaa5b0d016817a52b3892ab
JACA <carenas874@gmail.com>
2025-10-21 12:46:31 -0500

    configuración de login con el backend y implementación de TOKEN de inicio de seción usandolo UNICAMENTE POR AHORA en el home

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,11 @@
 import routes from "@routes/routes"
 import express, { type Application } from "express"
 import morgan from "morgan"
+import cors from "cors"
 
 const app: Application = express()
 
+app.use(cors()) // para comunicar el front con el back
 app.use(express.json())
 app.use(morgan("dev"))
 
